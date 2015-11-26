5f762e2994ef1d5b77a90d6db2cdb61e85f77936
Cole Willsea <coleww@gmail.com>
2015-11-26 07:00:40 -0800

    nice wow

--- parser.js
+++ parser.js
@@ -1,3 +1,8 @@
 var fs = require('fs')
 var tipots = require('this-is-probably-ok-to-say')
-
+var path = './corpuses/poems'
+fs.readdirSync(path).forEach(function (f) {
+  var dats = 'module.exports = ' + JSON.stringify(fs.readFileSync(path + '/' + f).toString().split("\n"), null, 2)
+  fs.writeFileSync(path + '/' + f + '.js', dats)
+  fs.unlinkSync(path + '/' + f)
+})
